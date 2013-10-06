430183ca430a2bf24a182d2f59b65d242ffb7a9f
Jacky.Song <jackyhopeful@gmail.com>
2013-10-06 20:45:01 +0800

    1

---
+++ DDZProj/ConsoleMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DDZProj
+{
+    public partial class ConsoleMain : Form
+    {
+        private Main _mainForm;
+        public ConsoleMain()
+        {
+            InitializeComponent();
+        }
+
+        public ConsoleMain(Main mainForm)
+        {
+            _mainForm = mainForm;
+
+            InitializeComponent();
+        }
+
+
+    }
+}
